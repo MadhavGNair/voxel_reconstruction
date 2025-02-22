43fba92407cb44daf191e9862003516b6e2cc487
Madhav Girish Nair <44905647+MadhavGNair@users.noreply.github.com>
2025-02-22 18:30:08 +0100

    feat: contour filling and HSV thresholding improved.

--- background_subtraction.py
+++ background_subtraction.py
@@ -9,7 +9,7 @@ class BackgroundSubtractor:
         self.width = 0
         self.background_model = None
 
-    def __create_background_model(self, var_threshold=16, random_state=42):
+    def __create_background_model(self, var_threshold=64):
         cap = cv2.VideoCapture(self.background_path)
 
         gmm = cv2.createBackgroundSubtractorMOG2(varThreshold=var_threshold, detectShadows=True)
@@ -24,7 +24,7 @@ class BackgroundSubtractor:
         cap.release()
 
         self.background_model = gmm
-
+    # 50, 10, 40
     def __background_subtraction(self, frame, h_threshold=50, s_threshold=10, v_threshold=40):
         gmm_mask = self.background_model.apply(frame, learningRate=0.0001)
         ret, gmm_mask = cv2.threshold(gmm_mask, 250, 255, cv2.THRESH_BINARY)
@@ -50,26 +50,42 @@ class BackgroundSubtractor:
 
         combined_mask = cv2.bitwise_and(gmm_mask, hsv_mask)
         
-        # kernel = np.ones((3,3), np.uint8)
-        # combined_mask = cv2.morphologyEx(combined_mask, cv2.MORPH_OPEN, kernel)
-        # combined_mask = cv2.morphologyEx(combined_mask, cv2.MORPH_CLOSE, kernel)
+        # apply morphological operations to remove noise
+        kernel = np.ones((5,5), np.uint8)
+        combined_mask = cv2.dilate(combined_mask, kernel, iterations=1)
+        combined_mask = cv2.erode(combined_mask, kernel, iterations=1)
 
-        contours, _ = cv2.findContours(combined_mask, cv2.RETR_CCOMP,cv2.CHAIN_APPROX_SIMPLE)
+        internal_contours, _ = cv2.findContours(combined_mask, cv2.RETR_CCOMP,cv2.CHAIN_APPROX_SIMPLE)
 
-        threshold_area = 500
+        # set the threshold area to the average of the two largest internal contours
+        internal_areas = [cv2.contourArea(contour) for contour in internal_contours]
+        internal_areas.sort()
+        if len(internal_areas) >= 2:
+            internal_threshold_area = internal_areas[-2] + (internal_areas[-1] - internal_areas[-2]) / 2
+        else:
+            internal_threshold_area = 7000
 
-        for contour in contours:
+        # fill holes inside foreground
+        for contour in internal_contours:
             area = cv2.contourArea(contour)
-            if area < threshold_area:
+            if area < internal_threshold_area:
                 cv2.drawContours(combined_mask, [contour], 0, 255, -1)
 
-        contours, _ = cv2.findContours(combined_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        external_contours, _ = cv2.findContours(combined_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        
+        # set the threshold area to the average of the two largest external contours
+        external_areas = [cv2.contourArea(contour) for contour in external_contours]
+        external_areas.sort()
+        if len(external_areas) >= 2:
+            external_threshold_area = external_areas[-2] + (external_areas[-1] - external_areas[-2]) / 2
+        else:
+            external_threshold_area = 7000
 
-        for contour in contours:
+        # remove pixels outside foreground
+        for contour in external_contours:
             area = cv2.contourArea(contour)
-            if area < threshold_area:
+            if area < external_threshold_area:
                 cv2.drawContours(combined_mask, [contour], -1, 0, cv2.FILLED)
-
         return combined_mask, frame
         
     def isolate_foreground(self, output_folder="./output"):
@@ -87,7 +103,7 @@ class BackgroundSubtractor:
         combined_mask, frame = self.__background_subtraction(frame)
         
         cv2.imshow('Combined Mask', combined_mask)
-        # cv2.imshow('Frame', frame)
+        cv2.imshow('Frame', frame)
         cv2.waitKey(0)
 
         cap.release()
